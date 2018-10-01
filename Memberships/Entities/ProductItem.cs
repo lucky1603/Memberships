2f7a4433ca232ffb94df39c257a0574df5642eec
Sinisa Ristic <sinisa.ristic@gmail.com>
2018-10-01 17:25:14 +0200

    Formiranje drop down lista.

---
+++ Memberships/Entities/ProductItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Memberships.Entities
+{
+    [Table("ProductItem")]
+    public class ProductItem
+    {
+        [Required]
+        [Key,Column(Order = 1)]
+        public int ProductId { get; set; }
+
+        [Required]
+        [Key, Column(Order = 2)]
+        public int ItemId { get; set; }
+
+        [NotMapped]
+        public int OldProductId { get; set; }
+
+        [NotMapped]
+        public int OldItemId { get; set; }
+    }
+}
---
+++ Memberships/Entities/Subscription.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Memberships.Entities
+{
+    [Table("Subscription")]
+    public class Subscription
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        [MaxLength(2048)]
+        public string Description { get; set; }
+
+        [MaxLength(20)]
+        [DisplayName("Registration Code")]
+        public string RegistrationCode { get; set; }
+
+    }
+}
