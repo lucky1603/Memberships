a4cc537759c555a78c683fffe3494adc4ae77126
Sinisa Ristic <sinisa.ristic@gmail.com>
2018-11-19 14:53:37 +0100

    Najnovije stanje.

--- Memberships/Controllers/HomeController.cs
+++ Memberships/Controllers/HomeController.cs
@@ -1,16 +1,32 @@
-﻿using System;
+﻿using Memberships.Extensions;
+using Microsoft.AspNet.Identity;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Threading.Tasks;
+using Memberships.Models;
+using System.Linq;
+using System;
 
 namespace Memberships.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var userId = Request.IsAuthenticated ? HttpContext.User.Identity.GetUserId() : null;
+            var thumbnails = await new List<ThumbnailModel>().GetProductThumbnailsAsync(userId);
+            var count = thumbnails.Count()/4;
+            var model = new List<ThumbnailAreaModel>();
+            for(int i = 0; i <= count; i++)
+            {
+                model.Add(new ThumbnailAreaModel
+                {
+                    Title = i.Equals(0) ? "My Content" : String.Empty,
+                    Thumbnails = thumbnails.Skip(i * 4).Take(4)
+                });
+            }
+
+            return View(model);
         }
 
         public ActionResult About()
---
+++ Memberships/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace Memberships.Extensions
+{
+    public static class HttpContextExtensions
+    {
+        private const string nameidentifier = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier";
+
+        public static string GetUserId(this HttpContextBase ctx)
+        {
+            string uid = String.Empty;
+            try
+            {
+                var claim = ctx.GetOwinContext().Get<ApplicationSignInManager>().AuthenticationManager.User.Claims.FirstOrDefault(c => c.Type.Equals(nameidentifier));
+                if(claim != default(Claim))
+                {
+                    uid = claim.Value;
+                }
+            } catch { }
+
+            return uid;
+        }
+    }
+}
